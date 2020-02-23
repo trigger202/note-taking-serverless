f2bde1c7aec3825ca872cbc6ed7e100f2bf99d75
ali fareh <farehh@gmail.com>
2020-02-23 12:03:21 +1100

    Logging with incognito working successfully

--- src/config.js
+++ src/config.js
@@ -1,16 +1,23 @@
+const region = 'us-east-2';
+const api_gate_way_url = "https://tssv01t70k.execute-api.us-east-2.amazonaws.com/dev";
+const bucket_name = 'note-taking-attachments';
+const USER_POOL_ID = 'us-east-2_blkBQuK6A';
+const APP_CLIENT_ID = '76j6d9e6pnbbsd67cja51g8jqb';
+const IDENTITY_POOL_ID = "us-east-2:c21aec24-f56d-4fbe-9757-970e4a156f72";
+
 export default {
     s3: {
-        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+        REGION: `${region}`,
+        BUCKET: `${bucket_name}`
     },
     apiGateway: {
-        REGION: "YOUR_API_GATEWAY_REGION",
-        URL: "YOUR_API_GATEWAY_URL"
+        REGION: `${region}`,
+        URL: `${api_gate_way_url}`,
     },
     cognito: {
-        REGION: "YOUR_COGNITO_REGION",
-        USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-        APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+        REGION: `${region}`,
+        USER_POOL_ID: `${USER_POOL_ID}`,
+        APP_CLIENT_ID: `${APP_CLIENT_ID}`,
+        IDENTITY_POOL_ID: `${IDENTITY_POOL_ID}`
     }
 };
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+import { Auth } from "aws-amplify";
 
-export default function Login() {
+export default function Login(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log("email", email);
+        console.log("password", password);
+        ///user cognito login 
+
+        try {
+            await Auth.signIn(email, password);
+            alert("Logged in");
+        } catch (e) {
+            alert(e.message);
+        }
+
+    }
+
     return (
-        <div>
-            Login page
+        <div className="Login">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                </FormGroup>
+                <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                    Login
+        </Button>
+            </form>
         </div>
-    )
+    );
 }
